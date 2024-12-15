bf45844df95ce8745d91736a14bc2f0c48708f04
Zhetpisbay070 <111482031+Zhetpisbay070@users.noreply.github.com>
2024-12-15 14:57:24 +0400

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main (){
-	fmt.Println("salamhello")
+
+
+
+
+func main() {
+	//ширина1, длина1 — параметры первого прямоугольника, ширина2, длина2 — параметры второго
+	//return bool показывает, может ли первый прямоугольник содержать второй, используя ТОЛЬКО один оператор «if»
+
+	canHold
+
+
+
+	func canHold(w1 int, l1 int, w2 int, l2 int)  bool {
+	rectangle1 := w1 * l1
+	rectangle2 := w2 * l2
+	result = rectangle1 > rectangle 2
+	return result
+
+
+	}
 }
+	//if rectangle1 > rectangle2 {
+	//	fmt.Println("Vmeshaet")
+	//} else if rectangle1 < rectangle2 {
+	//	fmt.Println("ne vmeshaet")
+
+
+	//a := 4.0
+	//b := 5.0
+	//stepen := math.Pow(a, b)
+	//fmt.Println(stepen)
+
+
+
+//for i := 1; i <= 10; i++ {
+//	if i == 5 {
+//		break
+//	}
+//	fmt.Println(i)
+//
+//}
+
+//	fmt.Println(fibonacci(5))
+//}
+//
+////func fibonacci(n int) int {
+////	var res int
+////	f1 := 0
+////	f2 := 1
+////	f3 := f1 + f2
+////	res = f2 + f3
+////
+////	for i := 2; i <= 5; i++ {
+////		fmt.Println()
+////	}
+////
+////	//return n-th number from fibonacci sequence
+////	return res
+////}
